e4f7368bc8def405868c120cf425330ff56609b1
Michelle <mpuschkarow@web.de>
2025-04-23 18:05:05 +0200

    to see image from begin on: requestAnimationFrame

--- js/classes/world.class.js
+++ js/classes/world.class.js
@@ -12,6 +12,7 @@ class World {
     }
 
     draw(){
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        requestAnimationFrame( () => this.draw());
     }
 }
