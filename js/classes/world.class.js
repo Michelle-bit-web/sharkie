d58e15967307dfc604ae3dc214f7be5edf4008be
Michelle <mpuschkarow@web.de>
2025-04-23 18:27:34 +0200

    clear canvas to avoid doubles of image & function to show enemy imgages

--- js/classes/world.class.js
+++ js/classes/world.class.js
@@ -5,14 +5,20 @@ class World {
         new Fish(),
         new Fish(),
     ];
+    canvas;
     ctx;
     constructor(canvas){
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
     draw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
         requestAnimationFrame( () => this.draw());
+        this.enemies.forEach(enemy =>{
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        })
     }
 }
