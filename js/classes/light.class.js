d42a55adbdf2cd12ac946a0988eda5863b6c2ab4
Michelle <mpuschkarow@web.de>
2025-04-24 09:01:43 +0200

    backgrounds added

---
+++ js/classes/light.class.js
@@ -0,0 +1,10 @@
+class Light extends MovableObject{
+    y = 0;
+    x = 0;
+    height = 480;
+    width = 720;
+    constructor(){
+        super().loadImage('../assets/img/3. Background/Layers/1. Light/1.png');
+        
+    }
+}
--- js/classes/world.class.js
+++ js/classes/world.class.js
@@ -5,10 +5,11 @@ class World {
         new Fish(),
         new Fish(),
     ];
-    water = [
-        new Water()
+    light = [
+        new Light()
     ];
     backgroundobjects = [
+        new BackgroundObject('../assets/img/3. Background/Layers/5. Water/D1.png', 0),
         new BackgroundObject('../assets/img/3. Background/Layers/4.Fondo 2/D1.png', 0),
         new BackgroundObject('../assets/img/3. Background/Layers/3.Fondo 1/D1.png', 0),
         new BackgroundObject('../assets/img/3. Background/Layers/2. Floor/D1.png', 0),
@@ -23,10 +24,10 @@ class World {
 
     draw(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.addObjectsToMap(this.water);
         this.addObjectsToMap(this.backgroundobjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.light);
 
         requestAnimationFrame( () => this.draw());
     }
