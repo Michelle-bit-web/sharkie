f9d3e4644abe2dd2d6a56465ce0e7caa78056d0c
Michelle <mpuschkarow@web.de>
2025-04-24 19:40:10 +0200

    switching background images with for-loop & js-classes restructured for backgrounds

--- js/classes/world.class.js
+++ js/classes/world.class.js
@@ -7,27 +7,33 @@ class World {
         new JellyFish(),
         new JellyFish(),
     ];
-    light = [
-        new Light()
-    ];
-    water = [
-        new Water()
-    ];
-    backgroundobjects = [
-        new BackgroundObject('../assets/img/3. Background/Layers/4.Fondo 2/D1.png', 0),
-        new BackgroundObject('../assets/img/3. Background/Layers/3.Fondo 1/D1.png', 0),
-        new BackgroundObject('../assets/img/3. Background/Layers/2. Floor/D1.png', 0),
-    ];
+    backgroundobjects = [];
     canvas;
     ctx;
     keyboard;
     camera_x = 0;
+
     constructor(canvas, keyboard){
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
         this.keyboard = keyboard;
+        this.collectBgImages();
         this.draw();
         this.setWorld();
+    }
+
+    collectBgImages(){
+        for (let i = 0; i <= 4; i++) {
+            let position = 719 * i;
+            let number = i % 2 === 1 ? 1 : 2;
+            this.backgroundobjects.push(
+                new BackgroundObject(`../assets/img/3. Background/Layers/5. Water/D${number}.png`, position),
+                new BackgroundObject(`../assets/img/3. Background/Layers/4.Fondo 2/D${number}.png`, position),
+                new BackgroundObject(`../assets/img/3. Background/Layers/3.Fondo 1/D${number}.png`, position),
+                new BackgroundObject(`../assets/img/3. Background/Layers/2. Floor/D${number}.png`, position),
+                new BackgroundObject(`../assets/img/3. Background/Layers/1. Light/${number}.png`, position),
+            )
+        }
     }
 
     setWorld(){
@@ -37,11 +43,9 @@ class World {
     draw(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.translate(this.camera_x, 0);
-        this.addObjectsToMap(this.water);
         this.addObjectsToMap(this.backgroundobjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
-        this.addObjectsToMap(this.light);
         this.ctx.translate(-this.camera_x, 0);
 
         requestAnimationFrame( () => this.draw());
