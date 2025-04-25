f201c9966df149eb7a4e1cea1fdaf01e410045b1
Michelle <mpuschkarow@web.de>
2025-04-25 16:14:51 +0200

    drawFrame just to instanceof character and enemies

--- js/classes/movable-object.class.js
+++ js/classes/movable-object.class.js
@@ -1,61 +1,61 @@
 class MovableObject {
-    x = 10;
-    y = 180;
-    img;
-    width = 70;
-    height = 90;
-    speed = 0.25;
-    currentImage = 0;
-    imageCache = {};
-    otherDirection = false;
+  x = 10;
+  y = 180;
+  img;
+  width = 70;
+  height = 90;
+  speed = 0.25;
+  currentImage = 0;
+  imageCache = {};
+  otherDirection = false;
 
-    loadImage(path){
-        this.img = new Image();
-        this.img.src = path;
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx) {
+    if (this instanceof Character || this instanceof Fish || this instanceof JellyFish || this instanceof Endboss) {
+      ctx.beginPath();
+      ctx.lineWidth = "3";
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
     }
+  }
 
-    loadImages(arr){
-        arr.forEach(path => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+  moveRight() {
+    this.x += this.speed;
+  }
 
-    draw(ctx){
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
+  moveLeft() {
+    this.x -= this.speed;
+  }
 
-    drawFrame(ctx){
-        ctx.beginPath();
-        ctx.lineWidth = '3';
-        ctx.strokeStyle = 'blue';
-        ctx.rect(this.x, this.y, this.width, this.height);
-        ctx.stroke();
-    }
+  moveUp() {
+    setInterval(() => {
+      this.y -= this.speed;
+    }, 1000 / 60);
+  }
 
-    moveRight(){
-        this.x += this.speed;
-    }
+  jump() {}
 
-    moveLeft(){
-        this.x -= this.speed;
-    }
-
-    moveUp(){
-        setInterval(() =>{
-            this.y -= this.speed;
-        }, 1000 / 60)
-    }
-
-    jump(){
-
-    }
-
-    playAnimation(images){
-        let i = this.currentImage % images.length;
-        let path = images[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
-    }
+  playAnimation(images) {
+    let i = this.currentImage % images.length;
+    let path = images[i];
+    this.img = this.imageCache[path];
+    this.currentImage++;
+  }
 }
