b0cfb008a1ed36bd1c8fdf561f0d8232854d2e60
Michelle <mpuschkarow@web.de>
2025-04-25 14:11:33 +0200

    edit differend animated images for resting state

--- js/classes/character.class.js
+++ js/classes/character.class.js
@@ -14,10 +14,31 @@ class Character extends MovableObject {
     "../assets/img/1.Sharkie/3.Swim/5.png",
     "../assets/img/1.Sharkie/3.Swim/6.png",
   ];
+  IMAGES_RESTING = [
+    "../assets/img/1.Sharkie/1.IDLE/1.png",
+    "../assets/img/1.Sharkie/1.IDLE/2.png",
+    "../assets/img/1.Sharkie/1.IDLE/3.png",
+    "../assets/img/1.Sharkie/1.IDLE/4.png",
+    "../assets/img/1.Sharkie/1.IDLE/5.png",
+    "../assets/img/1.Sharkie/1.IDLE/6.png",
+    "../assets/img/1.Sharkie/1.IDLE/7.png",
+    "../assets/img/1.Sharkie/1.IDLE/8.png",
+    "../assets/img/1.Sharkie/1.IDLE/9.png",
+    "../assets/img/1.Sharkie/1.IDLE/10.png",
+    "../assets/img/1.Sharkie/1.IDLE/11.png",
+    "../assets/img/1.Sharkie/1.IDLE/12.png",
+    "../assets/img/1.Sharkie/1.IDLE/13.png",
+    "../assets/img/1.Sharkie/1.IDLE/14.png",
+    "../assets/img/1.Sharkie/1.IDLE/15.png",
+    "../assets/img/1.Sharkie/1.IDLE/16.png",
+    "../assets/img/1.Sharkie/1.IDLE/17.png",
+    "../assets/img/1.Sharkie/1.IDLE/18.png",
+  ];
 
   constructor() {
     super().loadImage("../assets/img/1.Sharkie/3.Swim/1.png");
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_RESTING);
     this.animate();
     this.applyGravity();
   }
@@ -36,20 +57,24 @@ class Character extends MovableObject {
     }, 1000 / 30);
 
     setInterval(() => {
-        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-          this.playAnimation(this.IMAGES_WALKING);
-        } 
-    }, 50);
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) { //this.world.keyboard.SPACE for jump
+        this.playAnimation(this.IMAGES_WALKING);
+      }else{
+        this.playAnimation(this.IMAGES_RESTING); 
+      };
+    }, 50);      
   }
 
   applyGravity(){
     setInterval(() => {
-      if(this.y < 130){
+      if(this.isAboveGround()){
         this.y -= this.speedY;
         this.speedY -= this.acceleration;
       }
     }, 1000 / 25)
   }
-
+isAboveGround(){
+  return this.y < 130;
+}
   jump() {}
 }
