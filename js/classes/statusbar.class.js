46d4816deef5ae28ab434f8d1e3ff10e7a0ba333
Michelle <mpuschkarow@web.de>
2025-04-27 12:15:44 +0200

    added function to show statusbar depending on energy percentage

--- js/classes/statusbar.class.js
+++ js/classes/statusbar.class.js
@@ -1,3 +1,38 @@
 class Statusbar extends DrawableObject{
-    
+    IMAGES = [
+        "../assets/img/4. Marcadores/orange/0_  copia.png",
+        "../assets/img/4. Marcadores/orange/20_  copia.png",
+        "../assets/img/4. Marcadores/orange40_  copia.png",
+        "../assets/img/4. Marcadores/orange/60_  copia.png",
+        "../assets/img/4. Marcadores/orange/80_  copia.png",
+        "../assets/img/4. Marcadores/orange/100_  copia.png",
+    ];
+    percentage = 100;
+
+    constructor(){
+        this.loadImages(IMAGES);
+    }
+
+    setPercentage(percentage){
+        this.percentage = percentage;
+        let path = this.IMAGES[this.resolveImageIndex()];
+        this.img = this.imageCache[path];
+    }
+
+    resolveImageIndex(){
+        if(this.percentage == 100){
+            return 5
+        }else if(this.percentage > 80){
+            return 4
+        }else if(this.percentage > 60){
+            return 3
+        }else if(this.percentage > 40){
+            return 2
+        }else if(this.percentage > 20){
+            return 1
+        }else{
+            return 0
+        }
+
+    }
 }
